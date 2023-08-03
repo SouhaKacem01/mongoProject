2ba16d4aae4100106636a9eb40845dbbdbcf0f4a
SouhaKacem01 <137355559+SouhaKacem01@users.noreply.github.com>
2023-08-03 11:55:41 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-const express= require ('express');
-const app =express();
+const express = require('express');
+const app = express();
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
-const uri =
-"mongodb+srv://souhakacem:Mina2018@scluster.rxhbzeu.mongodb.net/?retryWrites=true&w=majority"
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const uri = "mongodb+srv://souhakacem:Mina2018@scluster.rxhbzeu.mongodb.net/notesDB"
 //  "mongodb+srv://<username>:<password>@<cluster-name>.frbqx.mongodb.net/<database-name>?retryWrites=true&w=majority";
 
 async function connect() {
-  try {
-    await mongoose.connect(uri);
-    console.log("Connected to MongoDB");
-  } catch (error) {
-    console.error(error);
-  }
+    try {
+        await mongoose.connect(uri, { useNewUrlParser: true }, { useUnifiedTopology: true });
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 connect();
@@ -21,34 +23,35 @@ app.get('/',(req,res) =>{
     res.send('Hello World');
 })
 */
-app.get('/api/courses',(req,res) =>{
+app.get('/api/courses', (req, res) => {
     res.send([1, 2, 3]);
 })
 
-app.get('/api/posts/:year/:month',(req,res) =>{
+app.get('/api/posts/:year/:month', (req, res) => {
     res.send(req.params.year);
-    
+
 })
 
-const notesSchema={
-    title : String,
-    content : String
+const notesSchema = {
+    title: String,
+    content: String
 }
 
-const Note = mongoose.model("Note",notesSchema);
-app.get("/",(req,res) => {
+const Note = mongoose.model("Note", notesSchema);
+
+app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
 })
 
-app.post("/", function(req,res){
-    let newNote = new Note ({
-        title : req.body.title,
-        content : req.body.content
-    })
+app.post("/", function (req, res) {
+    let newNote = new Note({
+        title: req.body.title,
+        content: req.body.content
+    });
     newNote.save();
     res.redirect('/');
 })
 
 
 const port = process.env.PORT || 3000;
-app.listen(port,()=>console.log(`listening on the port ${port}...`));
+app.listen(port, () => console.log(`listening on the port ${port}...`));
